1b5566d058c4800016bdeb43d60695669faa2917
bipintripathi <bipintripathi786@gmail.com>
2024-09-03 10:48:39 +0530

    Final Cypto chart API Integration

---
+++ controllers/dashboardController.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+
+
+
+
+
+exports.index = async(req, res) => {
+    try{
+        const [response,response2] = await Promise.all([
+         axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1'),
+         axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1')
+        ]);
+
+        const top4data = response2.data;
+        
+        const data = response.data;
+        res.render('index', {
+            title: 'Dashboard',
+            coinData: data,
+            coin4Data: top4data
+        });
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+    
+}
+
+exports.price = async(req, res) => {
+    try{
+    const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1');
+    const data = response.data;    
+    res.render('price', {
+        title: 'Price',
+        coinData: data
+    });
+}
+    catch(error){
+        console.log(error);
+        
+    }
+}
+
+exports.exchange = async(req, res) => {
+    try{
+        const id = req.query.id;
+        // promise.all to make multiple requests
+        const [coinResponse,chartResponse] = await Promise.all([
+            axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${id}&order=market_cap_desc&per_page=1&page=`),
+            axios.get(`https://api.coingecko.com/api/v3/coins/${id}/ohlc?vs_currency=usd&days=1`)
+        ]);
+
+        // Extract the data from the response
+        const coinInfo = coinResponse.data[0];
+        
+        // Extract the chart data from the response
+        const chartData = chartResponse.data.map(dataPoint => ({
+           x: dataPoint[0], // time
+           y: [dataPoint[1],dataPoint[2],dataPoint[3],dataPoint[4]] // [open, high, low, close]
+        }));
+
+        coinInfo.ohlc = chartData;
+        console.log(coinInfo);
+
+        res.render('exchange', {
+            title: 'Exchange',
+            coinInfo
+        });
+
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+   
+}
